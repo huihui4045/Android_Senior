bd861f570a9732e59e66d7f89d1542f0815d29c8
千涯秋瑟 <1476265623@qq.com>
2017-05-25 22:22:43 +0800

    完成进度条

--- app/src/main/java/com/huihui/senior/funcation/paint/view/CircleProgressView.java
+++ app/src/main/java/com/huihui/senior/funcation/paint/view/CircleProgressView.java
@@ -1,6 +1,8 @@
 package com.huihui.senior.funcation.paint.view;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,18 +11,23 @@ import android.view.View;
 import android.widget.FrameLayout;
 
 import com.huihui.senior.R;
+import com.huihui.senior.funcation.paint.widget.CircleProgressBar;
 
 /**
  * Created by molu_ on 2017/5/22.
  */
 
-public class CircleProgressView extends FrameLayout {
+public class CircleProgressView extends FrameLayout implements Handler.Callback {
+    private CircleProgressBar mCircleProgressBar;
+
+    private Handler mHandler = new Handler(this);
+
     public CircleProgressView(@NonNull Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CircleProgressView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,-1);
+        this(context, attrs, -1);
     }
 
     public CircleProgressView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
@@ -28,7 +35,43 @@ public class CircleProgressView extends FrameLayout {
 
         View view = View.inflate(context, R.layout.item_circle_progress, this);
 
+        mCircleProgressBar = ((CircleProgressBar) view.findViewById(R.id.bar));
+
+        mCircleProgressBar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mHandler.sendEmptyMessageDelayed(200, 200);
+            }
+        });
 
 
+    }
+
+    private int progress = 0;
+
+    @Override
+    public boolean handleMessage(Message msg) {
+
+
+        if (msg.what == 200) {
+
+            mCircleProgressBar.setProgress(progress);
+
+            progress++;
+
+            if (progress <=100) {
+
+                mHandler.sendEmptyMessageDelayed(200, 200);
+            } else {
+
+                mHandler.removeCallbacksAndMessages(null);
+            }
+
+
+        }
+
+
+        return false;
     }
 }
