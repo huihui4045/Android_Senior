a19f6b2a5ce22a73388080a3aa694abf52f626d8
千涯秋瑟 <1476265623@qq.com>
2017-06-20 19:49:39 +0800

    属性动画

--- app/src/main/java/com/huihui/senior/funcation/animator/view/AnimatorLayout.java
+++ app/src/main/java/com/huihui/senior/funcation/animator/view/AnimatorLayout.java
@@ -1,8 +1,6 @@
 package com.huihui.senior.funcation.animator.view;
 
-import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -41,14 +39,130 @@ public class AnimatorLayout extends FrameLayout implements View.OnClickListener 
     private void startAnimation() {
 
         //mImageDuo.setTranslationX();
-       // mImageDuo.setBackgroundColor();
-       // ObjectAnimator animator=ObjectAnimator.ofFloat(mImageDuo,"translationX",0f,300f);
+        // mImageDuo.setBackgroundColor();
+        // ObjectAnimator animator=ObjectAnimator.ofFloat(mImageDuo,"translationX",0f,300f);
 
-        ObjectAnimator animator=ObjectAnimator.ofInt(mImageDuo,"backgroundColor", Color.RED,Color.GREEN);
+       /* ObjectAnimator animator=ObjectAnimator.ofInt(mImageDuo,"backgroundColor", Color.RED,Color.GREEN);
 
         animator.setDuration(1000);
 
-        animator.start();
+        animator.start();*/
+
+        //多个动画同时执行
+
+
+       /* final ObjectAnimator animator=ObjectAnimator.ofFloat(mImageDuo,"alpha",0f,1f);
+
+        *//*animator.setDuration(300);
+        animator.setRepeatCount(2);
+        animator.setRepeatMode(ValueAnimator.REVERSE);*//*
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                float animatedFraction = animation.getAnimatedFraction();//执行百分比
+
+             float value= (float) animator.getAnimatedValue();
+
+                mImageDuo.setScaleX(0.5f+value);
+                mImageDuo.setScaleY(0.5f+value);
+
+                mImageDuo.setTranslationX(value*100);
+
+
+
+            }
+        });
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+            }
+        });
+
+        animator.start();*/
+
+
+       /* ValueAnimator valueAnimator=ValueAnimator.ofFloat(0,200f);
+        valueAnimator.setDuration(500);
+
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value= (float) animation.getAnimatedValue();
+                mImageDuo.setScaleX(0.5f+value);
+                mImageDuo.setScaleY(0.5f+value);
+
+                mImageDuo.setTranslationX(value*100);
+            }
+        });*/
+
+
+        //方法三
+
+        /*PropertyValuesHolder holder1=PropertyValuesHolder.ofFloat("alpha",1f,0.5f);
+        PropertyValuesHolder holder2=PropertyValuesHolder.ofFloat("scaleX",1f,0.5f);
+        PropertyValuesHolder holder3=PropertyValuesHolder.ofFloat("scaleY",1f,0.5f);
+
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(mImageDuo, holder1, holder2, holder3);
+
+        animator.setDuration(300);
+        animator.start();*/
+
+        //方法四
+
+        /*ObjectAnimator animator1=ObjectAnimator.ofFloat(mImageDuo,"translationX",0f,200f);
+        ObjectAnimator animator2=ObjectAnimator.ofFloat(mImageDuo,"alpha",0f,1f);
+        ObjectAnimator animator3=ObjectAnimator.ofFloat(mImageDuo,"scaleX",0f,2f);
+
+        animator1.setStartDelay(1000);
+
+        AnimatorSet animatorSet=new AnimatorSet();
+
+        animatorSet.setDuration(1000);
+        animatorSet.playTogether(animator1,animator2,animator3);
+       // animatorSet.playSequentially(animator1,animator2,animator3);
+
+        animatorSet.start();*/
+
+        //.......................自由落体运动
+
+       /* ValueAnimator animator = new ValueAnimator();
+
+        animator.setDuration(4000);
+        animator.setObjectValues(new PointF(0, 0));
+
+        final PointF pointF = new PointF();
+
+        animator.setEvaluator(new TypeEvaluator<PointF>() {
+            @Override
+            public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
+                //
+
+                pointF.x = 100f * fraction * 10;
+
+                pointF.y = 0.5f * 29f * (fraction * 10) * (fraction * 10);
+
+
+                return pointF;
+            }
+        });
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+
+                PointF pointF1 = (PointF) animation.getAnimatedValue();
+                mImageDuo.setX(pointF1.x);
+                mImageDuo.setY(pointF1.y);
+            }
+        });
+
+        animator.start();*/
+
 
     }
 
